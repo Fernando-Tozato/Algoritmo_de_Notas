9ff3be012f45dc52743f505a887778194ddd7f46
Fernando Tozato <101608124+Fernando-Tozato@users.noreply.github.com>
2022-07-17 16:42:07 -0300

    pronto

--- funcoes.py
+++ funcoes.py
@@ -6,26 +6,6 @@ import os
 #database = 'notas'
 #duas_etapas = 'sua_senha'
 
-# Pergunta ao usuário qual operação vai ser efetuada
-def escolhe_operacao():
-    escolha_ok = True
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("Seja bem-vindo! O que você gostaria de fazer?")
-    while escolha_ok:
-        print("""
-        1 - Olhar as notas.
-        2 - Inserir matérias.
-        3 - Calcular CR.
-        """)
-        escolha = input("--> ")
-        
-        if not (escolha == '1' or escolha == '2' or escolha == '3' or escolha == 'admin'):
-            print("Operação inválida, tente novamente.\n")
-        else:
-            escolha_ok = False
-    
-    return escolha
-
 # Pega a tabela do banco de dados e imprime ao usuário
 def mostra_notas(host,user,pw,database):
     # Pega os dados
@@ -43,12 +23,12 @@ def mostra_notas(host,user,pw,database):
         leituras.append(i)
     
     # Cria um DataFrame com Panda
-    colunas = ['ID','Matéria','Cód.','Carga','AV1','AVA1','AV2','AVA2','AV3','AVD','AVDS','Média']
+    colunas = ['ID','Matéria','Cód.','Carga','AV1','AV2','AV3','AVA1','AVA2','AVD','AVDS','Média']
     df = pd.DataFrame(leituras, columns = colunas)
     return df
 
 # Calcula a média
-def calcula_media(av1,ava1,av2,ava2,av3,avd,avds):
+def calcula_media(av1,av2,av3,ava1,ava2,avd,avds):
     soma = 0
     av1 += ava1
     av2 += ava2
@@ -64,37 +44,19 @@ def calcula_media(av1,ava1,av2,ava2,av3,avd,avds):
         else: soma += av3
     if avd > avds: soma += avd
     else: soma += avds
-    media = round(soma / 3,1)
+    media = round(soma / 3,2)
     return media
 
 # Preenche a tabela com as informções que o usuário insere
-def insere_materias(host,user,pw,database):
-    os.system('cls' if os.name == 'nt' else 'clear')
-    # Pergunta ao usuário quantas matérias serão inseridas
-    num = int(input("Quantas matérias você gostaria de inserir? --> "))
+def insere_materias(host,user,pw,database,materia,codigo,av1,av2,av3,ava1,ava2,avd,avds):
+    media = calcula_media(av1,av2,av3,ava1,ava2,avd,avds)
     
-    for i in range(num):
-        print("Matéria número",i)
-        # Define todas as informações da tabela e imprime a média ao usuário
-        os.system('cls' if os.name == 'nt' else 'clear')
-        materia = input("Insira o nome completo da matéria: ")
-        codigo = input("Insira o código da matéria: ")
-        carga = 80
-        av1 = round(float(input("Insira a nota da AV1. Caso não tenha feito, coloque 0: ")),2)
-        av2 = round(float(input("Insira a nota da AV2. Caso não tenha feito, coloque 0: ")),2)
-        av3 = round(float(input("Insira a nota da AV3. Caso não tenha feito, coloque 0: ")),2)
-        ava1 = round(float(input("Insira a nota da AVA1. Caso não tenha feito, coloque 0: ")),2)
-        ava2 = round(float(input("Insira a nota da AVA2. Caso não tenha feito, coloque 0: ")),2)
-        avd = round(float(input("Insira a nota da AVD. Caso não tenha feito, coloque 0: ")),2)
-        avds = round(float(input("Insira a nota da AVDS. Caso não tenha feito, coloque 0: ")),2)
-        media = calcula_media(av1,ava1,av2,ava2,av3,avd,avds)
-        
-        # Cria os comandos em SQL
-        inserir_dados = f"insert into notas(materia,codigo,carga,av1,av2,av3,ava1,ava2,avd,avds,media) value ('{materia}','{codigo}',{carga},{av1},{av2},{av3},{ava1},{ava2},{avd},{avds},{media})"
-        
-        # Faz a conexão com a DB e comita os comandos
-        connection = create_db_connection(host,user,pw,database)
-        execute_query(connection,inserir_dados)
+    # Cria os comandos em SQL
+    inserir_dados = f"insert into notas(materia,codigo,carga,av1,av2,av3,ava1,ava2,avd,avds,media) value ('{materia}','{codigo}',80,{av1},{av2},{av3},{ava1},{ava2},{avd},{avds},{media})"
+    print(inserir_dados)
+    # Faz a conexão com a DB e comita os comandos
+    connection = create_db_connection(host,user,pw,database)
+    execute_query(connection,inserir_dados)
 
 # Calcula o CR usando a DB
 def calcula_cr(host,user,pw,database):
@@ -111,95 +73,9 @@ def calcula_cr(host,user,pw,database):
     for leitura in leituras:
         soma += float(leitura[11]) * 80
     cr = round(soma / (int(leituras[-1][0]) * 80),2)
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("Seu CR é:",cr,"\nCálculo feito a partir de",leituras[-1][0],"matérias.")
+    return f"CR: {cr}.  De {leituras[-1][0]} matérias cursadas."
 
-# Verifica se o usuário é admin
-def verifica_admin(duas_etapas):
-    # Inicia uma verificação em duas etapas com senha definida no topo do código
-    count = 0
-    os.system('cls' if os.name == 'nt' else 'clear')
-    enter = input("Insira a senha de verificação em duas etapas: ")
-    if enter == duas_etapas:
-        return True
-    while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        enter = input("Senha incorreta, tente novamente: ")
-        if enter == duas_etapas:
-            return True
-        # Limita o número de tentativas a três no total
-        count += 1
-        if count >= 2: 
-            print('Números de tentativas excedido, tente novamente mais tarde.')
-            return False
-
-# Permite criar uma db e comitar
-def modo_admin(host,user,pw,database):
-    # Escolhe o tipo de operção a ser realizada
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print('O que vocÊ deseja fazer?')
-    escolha_ok = True
-    while escolha_ok:
-        print("""
-        1 - Criar uma DataBase
-        2 - Escrever meu próprio código
-        """)
-        escolha = input("--> ")
-        
-        if not (escolha == '1' or escolha == '2'):
-            print("Operação inválida, tente novamente.\n")
-        else:
-            escolha_ok = False
-    
-    # Cria uma db
-    if escolha == '1':
-        os.system('cls' if os.name == 'nt' else 'clear')
-        nome_db = input('Digite o nome da DataBase: ')
-        criar_db = (f"CREATE DATABASE '{nome_db}';")
-        connection = create_server_connection(host,user,pw)
-        create_database(connection,criar_db)
-        
-    # Executa comandos definidos pelo usuário
-    elif escolha == '2':
-        os.system('cls' if os.name == 'nt' else 'clear')
-        database = input("Digite o nome da DataBase a ser editada: ")
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("Digite os comandos a serem executados (escreva tudo em uma linha, só dê enter quando terminar.)")
-        comandos = input("> ")
-        connection = create_db_connection(host,user,pw,database)
-        execute_query(connection,comandos)
-        
-# Pergunta se o usuário quer realizar outra operação
-def repete_ou_fim():
-    print("\n\nVocê gostaria de executar mais alguma operação?")
-    escolha_ok = True
-    while escolha_ok:
-        print("""
-        1 - Olhar as notas.
-        2 - Inserir matérias.
-        3 - Calcular CR.
-        4 - Encerrar o programa.
-        """)
-        escolha = input("--> ")
-        
-        if not (escolha == '1' or escolha == '2' or escolha == '3' or escolha == '4' or escolha == 'admin'):
-            print("Operação inválida, tente novamente.\n")
-        else:
-            escolha_ok = False
-    
-    if escolha == '1':
-        mostra_notas()
-        repete_ou_fim()
-    elif escolha == '2':
-        insere_materias()
-        repete_ou_fim()
-    elif escolha == '3':
-        calcula_cr()
-        repete_ou_fim()
-    elif escolha == '4':
-        print("Programa encerrado com sucesso.")
-        return
-    elif escolha == 'admin': 
-        if verifica_admin():
-            modo_admin()
-            repete_ou_fim()
+def cria_tabela(host,user,pw,nome_db):
+    criar_db = (f"CREATE DATABASE {nome_db};")
+    connection = create_server_connection(host,user,pw)
+    create_database(connection,criar_db)
--- main.py
+++ main.py
@@ -1,21 +1,169 @@
-from banco_de_dados import *
+import PySimpleGUI as sg
 from funcoes import *
+from banco_de_dados import *
 
-# A fazer:
-# - Fazer um display
+global host,user,database,pw
 
-escolha = escolhe_operacao()
+# Define o layout das janelas
+def janela_inicio():
+    sg.theme('DarkBlack')
+    layout = [
+        [sg.Text('Seja bem-vindo! O que você gostaria de fazer?')],
+        [sg.Button('Olhar as notas')],
+        [sg.Button('Inserir matérias')],
+        [sg.Button('Admin')]
+    ]
+    return sg.Window('Início',layout,finalize='true')
 
-if escolha == '1':
-    mostra_notas()
-    repete_ou_fim()
-elif escolha == '2':
-    insere_materias()
-    repete_ou_fim()
-elif escolha == '3':
-    calcula_cr()
-    repete_ou_fim()
-elif escolha == 'admin':
-    if verifica_admin():
-        modo_admin()
-        repete_ou_fim()
+def janela_login():
+    sg.theme('DarkBlack')
+    layout = [
+        [sg.Text('Para continuar, insira algumas informações: ')],
+        [sg.Text('Host: '),sg.Input(key='host',size=(20,1))],
+        [sg.Text('Usuário: '),sg.Input(key='user',size=(20,1))],
+        [sg.Text('Banco de Dados: '),sg.Input(key='database',size=(20,1))],
+        [sg.Text('Senha: '),sg.Input(key='senha',password_char='*',size=(20,1))],
+        [sg.Button('Entrar')]
+    ]
+    return sg.Window('Login',layout,finalize='true')
+
+def janela_erro():
+    sg.theme('DarkBlack')
+    layout = [
+        [sg.Text('Para continuar, insira algumas informações: ')],
+        [sg.Text('Host: '),sg.Input(key='host',size=(20,1))],
+        [sg.Text('Usuário: '),sg.Input(key='user',size=(20,1))],
+        [sg.Text('Banco de Dados: '),sg.Input(key='database',size=(20,1))],
+        [sg.Text('Senha: '),sg.Input(key='senha',password_char='*',size=(20,1))],
+        [sg.Button('Entrar')],
+        [sg.Text('Informações erradas, por favor tente novamente.')]
+    ]
+    return sg.Window('Login',layout,finalize='true')
+
+def janela_tabela():
+    sg.theme('DarkBlack')
+    layout = [
+        [sg.Text(calcula_cr(host,user,pw,database))],
+        [sg.Text(mostra_notas(host,user,pw,database))],
+        [sg.Button('Voltar')]
+    ]
+    return sg.Window('Notas',layout,finalize='true')
+
+def janela_dados():
+    sg.theme('DarkBlack')
+    layout = [
+        [sg.Text('Se alguma das avaliações não foi realizada, coloque 0')],
+        [sg.Text('Matéria: '),sg.Input(key='materia',size=(50,1))],
+        [sg.Text('Código: '),sg.Input(key='codigo',size=(10,1))],
+        [sg.Text('AV1: '),sg.Input(key='av1',size=(4,1))],
+        [sg.Text('AV2: '),sg.Input(key='av2',size=(4,1))],
+        [sg.Text('AV3: '),sg.Input(key='av3',size=(4,1))],
+        [sg.Text('AVA1: '),sg.Input(key='ava1',size=(4,1))],
+        [sg.Text('AVA2: '),sg.Input(key='ava2',size=(4,1))],
+        [sg.Text('AVD: '),sg.Input(key='avd',size=(4,1))],
+        [sg.Text('AVDS: '),sg.Input(key='avds',size=(4,1))],
+        [sg.Button('Ok')]
+    ]
+    return sg.Window('Dados',layout,finalize='true')
+
+def janela_admin():
+    sg.theme('DarkBlack')
+    layout = [
+        [sg.Text('Criar Database:')],
+        [sg.Text('Nome: '),sg.Input(key='nome_db',size=(20,1))],
+        [sg.Button('Criar')],
+        [sg.Text('')],
+        [sg.Text('Escrever código:')],
+        [sg.Input(key='comandos',size=(100,100))],
+        [sg.Button('Commit')],
+        [sg.Button('Voltar')]
+    ]
+    return sg.Window('Admin',layout,finalize='true')
+
+# Cria a janela inicial
+janela1, janela2, janela3, janela4, janela5 = janela_inicio(), None, None, None, None
+
+# Cria um loop de leitura de eventos
+while True:
+    window,event,values = sg.read_all_windows()
+    
+    # Fecha as janelas
+    if window == janela1 and event == sg.WIN_CLOSED: break
+    if window == janela2 and event == sg.WIN_CLOSED: break
+    if window == janela3 and event == sg.WIN_CLOSED: break
+    if window == janela4 and event == sg.WIN_CLOSED: break
+    if window == janela5 and event == sg.WIN_CLOSED: break
+    
+    # Janela 1, janela de início com login
+    if window == janela1 and event == 'Olhar as notas':
+        escolha = 1
+        janela2 = janela_login()
+        janela1.hide()
+    
+    if window == janela1 and event == 'Inserir matérias':
+        escolha = 2
+        janela2 = janela_login()
+        janela1.hide()
+    
+    if window == janela1 and event == 'Admin':
+        escolha = 3
+        janela2 = janela_login()
+        janela1.hide()
+    
+    # Janelas 2 e 3, janelas de login e erro de login
+    if (window == janela2 or window == janela3) and event == 'Entrar':
+        if (window == janela2 or window == janela3) and not verifica_senha_connection(values['host'],values['user'],values['senha']):
+            janela3 = janela_erro()
+            janela2.hide()
+        else:
+            host = values['host']
+            user = values['user']
+            database = values['database']
+            pw = values['senha']
+            
+            if escolha == 1:
+                janela4 = janela_tabela()
+                janela2.hide()
+            
+            if escolha == 2:
+                janela4 = janela_dados()
+                janela2.hide()
+            
+            if escolha == 3:
+                if user == 'root':
+                    janela4 = janela_admin()
+                    janela2.hide()
+                else:
+                    janela3 = janela_erro()
+                    janela2.hide()
+    
+    # Janela 4, todas as janelas com operações
+    if window == janela4 and event == 'Voltar':
+        janela5 = janela_inicio()
+        janela4.hide()
+    
+    if window == janela4 and event == 'Ok':
+        insere_materias(host,user,pw,database,values['materia'],values['codigo'],float(values['av1']),float(values['av2']),float(values['av3']),float(values['ava1']),float(values['ava2']),float(values['avd']),float(values['avds']))
+        janela4.hide()
+        janela4 = janela_tabela()
+    
+    if window == janela4 and event == 'Criar':
+        cria_tabela(host,user,pw,values['nome_db'])
+    
+    if window == janela4 and event == 'Commit':
+        connection = create_db_connection(host,user,pw,database)
+        execute_query(connection,values['comandos'])
+    
+    # Janela 5, janela de início com login apenas para admin
+    if window == janela5 and event == 'Olhar as notas':
+        janela4 = janela_tabela()
+        janela5.hide()
+    
+    if window == janela5 and event == 'Inserir matérias':
+        janela4 = janela_dados()
+        janela5.hide()
+    
+    if window == janela5 and event == 'Admin':
+        escolha = 3
+        janela2 = janela_login()
+        janela5.hide()
